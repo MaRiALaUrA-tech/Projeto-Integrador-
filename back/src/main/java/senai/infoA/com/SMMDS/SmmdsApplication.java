5d107614b16b914d07638aa342342c18a2c9bdfb
gabriellegomesb <gabrielle.gomesbandeira15@gmail.com>
2026-03-18 11:12:22 -0300

    start project

---
+++ back/src/main/java/senai/infoA/com/SMMDS/SmmdsApplication.java
@@ -0,0 +1,13 @@
+package senai.infoA.com.SMMDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmmdsApplication.class, args);
+	}
+
+}
