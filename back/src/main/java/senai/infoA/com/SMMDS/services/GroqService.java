f1c07678e1f2597ffb2997f80009f5ab731fe8a0
MaRiALaUrA-tech <marialaurasantosramosmaria@gmail.com>
2026-05-22 09:27:54 -0300

    ajuste da classe GROQ

--- back/src/main/java/senai/infoA/com/SMMDS/services/GroqService.java
+++ back/src/main/java/senai/infoA/com/SMMDS/services/GroqService.java
@@ -1,8 +1,8 @@
+package senai.infoA.com.SMMDS.services;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class GroqService {
-    private final ChatClient chatClient;
 
-    public GroqService(ChatClie
 }
