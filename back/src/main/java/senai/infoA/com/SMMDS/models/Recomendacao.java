9f47028d080dec18b7e2159d96127f2bc0e2e5c1
MaRiALaUrA-tech <marialaurasantosramosmaria@gmail.com>
2026-05-22 10:52:56 -0300

    add letra na palavra

--- back/src/main/java/senai/infoA/com/SMMDS/models/Recomendacao.java
+++ back/src/main/java/senai/infoA/com/SMMDS/models/Recomendacao.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Recomenacao")
+@Table(name = "Recomendacao")
 public class Recomendacao{
 
     @Id
