dd3d35174096ff82080b0bd741f0b2c1b3004e97
Maria Laura <marialaurasantosramosmaria@gmail.com>
2026-06-08 11:30:40 -0300

    Delete Scriptatualizado.sql

--- Scriptatualizado.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE sensor DROP COLUMN tipo_sensor_id;
-drop table tipo_sensor;
-drop table  recomenacao;
-ALTER TABLE sensor DROP CONSTRAINT fk_tipo_sensor;
-ALTER TABLE sensor ADD COLUMN tipo_sensor varchar(30);
